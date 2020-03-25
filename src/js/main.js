f0a7cbe5a3e4255e5ce5322298194167164bb8d5
Left <left@left.tw>
2020-03-25 18:14:09 +0800

    add event

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,23 @@
 import '../sass/normalize.scss';
 import '../sass/theme.scss';
 
-$('#year').text(new Date().getFullYear());
+function validateEmail(email) {
+    // http://emailregex.com/
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var re_payment = /^[0-9-a-zA-Z_\-\.@]+$/;
+    return re.test(email) && re_payment.test(email);
+}
 
 var data = {
 	username: '',
 	token: '',
+	page: 'login',
 	overview: {
 		total: 30,
 		health: 28,
 		fever: 0,
 		warning: 1,
-		danger: 1,
-		status: 'wait'
+		danger: 1
 	},
 	log: [
 		{
@@ -44,24 +49,15 @@ var data = {
 };
 
 window.addEventListener('load', function () {
+	$('#year').text(new Date().getFullYear());
    var vue = new Vue({
 		el: '#vue',
 		data: data,
 		mounted: function() {
-			if(this.overview.danger > 0) {
-				this.overview.status = 'danger';
-				return ;
-			}
-
-			if(this.overview.warning > 0 || this.overview.fever > 0) {
-				this.overview.status = 'warning';
-				return ;
-			}
-
-			if(this.overview.total === this.overview.health) {
-				this.overview.status = 'success';
-				return ;
-			}
+			var c = this.$cookies.get('fio_health');
+		    if(c !== null) {
+		      this.page = 'dashboard';
+		    }
 		},
 		methods: {
 			toPercent: function(num, total){
@@ -74,24 +70,72 @@ window.addEventListener('load', function () {
 				
 	        },
 		    resetErrorMessage: function(id) {
-		        $(id).find('.form-control').removeClass('is-invalid is-valid')
-		             .end()
+		        $(id).removeClass('is-invalid is-valid')
 		             .find('.invalid-feedback').text('');
 		    },
 		    setErrorMessage: function(id, message) {
-		        $(id).find('.form-control').addClass('is-invalid')
-		             .end()
+		        $(id).addClass('is-invalid')
 		             .find('.invalid-feedback').text(message);
 		    },
+		    getStatus: function() {
+		    	//set status
+				if(this.overview.danger > 0) {
+					return 'danger';
+				}
+				if(this.overview.warning > 0 || this.overview.fever > 0) {
+					return 'warning';
+				}
+				if(this.overview.total === this.overview.health) {
+					return 'success';
+				}
+		    },
 		    login: function() {
-		    	console.log(2);
-		        
+		        // reset 
+				vue.resetErrorMessage('.auth-form');
+				var validate = true;
+
+				//validate
+				if(this.username === "") {
+				    validate = false;
+				    vue.setErrorMessage('#form-group-username', "Please enter Email.");
+				} else {
+				    if (!validateEmail(this.username)) {
+				        validate = false;
+				        vue.setErrorMessage('#form-group-username', "Pleaes confirm your email address.");
+				    }
+				}
+
+				if(validate) {
+					console.log(200);
+
+					///set cookie
+					this.$cookies.set('fio_health', {
+						'login': this.username,
+						'token': 1
+					}, '1d');
+
+					alert('login success.');
+					this.page = 'dashboard';
+
+				    // api('/login', data, function(result) {
+				    // }, function(result){
+				    //     vue.setErrorMessage('#form-group-username', result.data.message);
+				    // });
+				}
+		    },
+		    logout: function() {
+		    	this.$cookies.remove('fio_health');
+		    	this.username = '';
+		    	this.token = '';
+		    	this.page = 'login';
+		    },
+		    goPage: function(page) {
+		    	this.page = page;
+		    },
+		    showLog: function() {
+		    	this.page = 'log';
+		    	//todo: api
 		    }
 		}
 	});
-
-// window.vue = vue;
 })
-
-
-
